33de532a95a72804b259b699bf36fe224e106d6d
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-11 14:17:00 +0200

    tlo

---
+++ ustawTlo/ustawTlo.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var paragraf1 = document.getElementById('paragraf1');
+var paragraf2 = document.getElementById('paragraf2');
+
+function ustawTlo() {
+paragraf1.style.backgroundColor = "red";
+paragraf2.style.backgroundColor = "yellow";
+}
+
+guzik.onclick = ustawTlo;
